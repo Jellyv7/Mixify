edafb1202f96cb3e791d0f1acc6403d5336e9440
Jelly.flac <pinkshade1709@gmail.com>
2024-03-19 15:12:25 -0400

    Delete Spotify Image Generator directory

--- Spotify Image Generator/scripts/lib/index.js
+++
@@ -1,4 +0,0 @@
-export { fetchData, getAccessToken } from './API.js';
-export { redirectToAuthCodeFlow } from './AC_PKCE.js';
-export { salem } from './salem.js'; 
-export { metrics, terms, alignments, themes } from './props.js';
